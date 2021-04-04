fbb6eaa3c6f20bf893bcece24e1db753d3d555e4
Sondre Knutsen <sondreknutsen1@gmail.com>
2021-04-05 00:23:33 +0200

    removed old cors implementation

--- src/index.ts
+++ src/index.ts
@@ -22,16 +22,10 @@ const main = async () => {
 
     const app = express();
 
-    // app.use((req, res, next) => {
-    //     res.header("Access-Control-Allow-Origin", "*");
-    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    //     next();
-    // });
-
     app.use(cors({
         origin: 'http://localhost:3000',
         credentials: true
-    }))
+    }));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
